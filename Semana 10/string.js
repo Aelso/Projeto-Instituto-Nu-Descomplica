50a761df5ab92846e6446cdaa8263d74966e3e29
aelsoRochaBrito <49383092+Aelso@users.noreply.github.com>
2022-11-21 22:40:06 -0300

    Commint 21.11.22

---
+++ Semana 10/string.js
@@ -0,0 +1,81 @@
+
+var nome = 'Aelso Rocha Brito';
+var x = nome.length;
+console.log(x);
+
+resultado = x > 15 ? 'Obrigado' : 'Por favor preencha  seu nome completo';
+console.log(resultado);
+
+var res = nome.toLocaleUpperCase();
+console.log(res);
+
+var jogo = 'Barcelona vs Real melhor time';
+
+var posicao = jogo.indexOf('Real');
+
+console.log(posicao);
+
+var cortado = jogo.slice(0, 10);
+console.log(cortado);
+
+var val = jogo.includes('Real');
+console.log(val);
+
+var str1 = 'Hello';
+var str2 = ' Turma';
+
+var str3 = str1.concat(str2);
+console.log(str3);
+
+var frase = '          Olá  vc  aprendendo javascript   '
+var frasetam = frase.length;
+
+console.log(frasetam);
+
+console.log(frase.trim());
+
+var num = '1,2,3,4,5,6,7,8,9';
+
+var arr = num.split(',');
+console.log(arr);
+
+
+
+
+
+
+
+
+
+// https://www.w3schools.com/jsref/jsref_obj_string.asp
+// JavaScript String Methods
+//Name	Description
+//substr()	Extracts a number of characters from a string, from a start index (position)
+//substring()	Extracts characters from a string, between two specified indices (positions)
+//replace()	Searches a string for a value, or a regular expression, and returns a string where the values are replaced
+//search()	Searches a string for a value, or regular expression, and returns the index (position) of the match
+//toLocaleLowerCase()	Returns a string converted to lowercase letters, using the host's locale
+//toLocaleUpperCase()	Returns a string converted to uppercase letters, using the host's locale
+//indexOf()	Returns the index (position) of the first occurrence of a value in a string
+//includes()	Returns if a string contains a specified value
+//slice()	Extracts a part of a string and returns a new string
+//concat()	Returns two or more joined strings
+//lastIndexOf()	Returns the index (position) of the last occurrence of a value in a string
+//charCodeAt()	Returns the Unicode of the character at a specified index
+//trim()	Returns a string with removed whitespaces
+//charAt()	Returns the character at a specified index (position)
+//valueOf()	Returns the primitive value of a string or a string object
+//split()	Splits a string into an array of substrings
+//toString()	Returns a string or a string object as a string
+//match()	Searches a string for a value, or a regular expression, and returns the matches
+//length	Returns the length of a string
+
+
+
+
+
+
+
+
+
+
